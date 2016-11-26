dc8a09eac5f70478d925e9e8c72749c9a65befa8
Bjarte Larsen <bjartekll555@gmail.com>
2016-11-26 16:54:05 +0100

    Updated makefile and created task 3

---
+++ oppgave3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+  int age = 22;
+  std::cout << "Bjarte Larsen\n"
+            << "Fantasy land \n"
+            << "1354 Fantasy street\n"
+            << "Age: " << age << "\n"; 
+
+
+  return 0;
+}
